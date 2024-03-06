1cd2a5a9b6eb2c84eae356cd7c5f82cd583d3af8
Dheime Mocelin <dheime7@gmail.com>
2024-03-06 15:54:11 -0400

    Mudanças no código da API do CNPJ

--- README.md
+++ README.md
@@ -1,35 +1,44 @@
-# Consulta de CNPJ
+# Relatório Comitê de Crédito
 
-Este é um aplicativo simples para consulta de CNPJ. Ele permite que os usuários insiram um CNPJ, médias de faturamento mensal de anos atual e anterior, bem como o score da empresa. Com base nesses dados, o aplicativo consulta uma API para obter informações da empresa e calcula um novo limite liberado para empréstimos a partir do score e médias de faturamento fornecidas.
+Este é um aplicativo Streamlit para gerar relatórios do comitê de crédito para empresas e pessoas físicas.
 
 ## Funcionalidades
 
-- Consulta de informações da empresa com base no CNPJ fornecido.
-- Cálculo do novo limite liberado para empréstimos com base no score e médias de faturamento.
-- Geração de um PDF com os dados da empresa e do novo limite liberado.
+### Para Empresas
 
-## Tecnologias Utilizadas
+- Consulta de dados da empresa com base no CNPJ inserido.
+- Exibição dos dados da empresa, incluindo razão social, tipo de empresa, data de abertura, média do faturamento mensal do ano atual e anterior, SCORE e risco.
+- Geração de um PDF contendo os dados da empresa.
 
-- Python
-- Streamlit: Biblioteca para construção de aplicativos web com Python.
-- Pandas: Biblioteca para manipulação e análise de dados.
-- Numpy: Biblioteca para computação numérica.
-- Requests: Biblioteca para fazer requisições HTTP em Python.
-- FPDF: Biblioteca para geração de documentos PDF.
+### Para Pessoa Física
 
-## Como Utilizar
+- Entrada do nome, CPF e quantidade de animais de uma pessoa física.
+- Cálculo do índice de risco e valor liberado com base na quantidade de animais.
+- Geração de um PDF contendo os dados da pessoa física.
+
+## Como usar
 
 1. Clone este repositório.
 2. Instale as dependências listadas no arquivo `requirements.txt`.
-3. Execute o arquivo `app.py`.
-4. Insira o CNPJ da empresa, médias de faturamento mensal dos anos atual e anterior, e o score da empresa.
-5. Clique no botão "Consultar" para obter as informações da empresa e calcular o novo limite liberado para empréstimos.
-6. Um PDF será gerado com os dados da empresa e do novo limite liberado. Você pode baixá-lo clicando no botão "Baixe o PDF".
+3. Execute o aplicativo usando o comando `streamlit run app.py`.
+4. Insira o CNPJ ou CPF, dependendo do tipo de consulta desejada.
+5. Insira os dados solicitados e clique no botão correspondente para consultar e gerar o relatório em PDF.
+
+## Dependências
+
+- `requests`: Para fazer solicitações HTTP à API de consulta de CNPJ.
+- `pandas`: Para manipulação de dados.
+- `numpy`: Para cálculos matemáticos.
+- `streamlit`: Para criar a interface do aplicativo web.
+- `datetime`: Para manipulação de datas e horários.
+- `json`: Para trabalhar com dados JSON.
+- `base64`: Para codificar e decodificar dados em base64.
+- `fpdf`: Para gerar documentos PDF.
 
 ## Autor
 
-[Dheime Mocelin]
+Este aplicativo foi desenvolvido por [Dheime Mocelin].
 
 ## Licença
 
-Este projeto está sob a [Licença MIT](LICENSE).
+Este projeto está licenciado sob a [Licença MIT](https://opensource.org/licenses/MIT).
--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def get_company_data(cnpj):
     
     try:
         # Fazendo a solicitação GET à API
-        response = requests.get(url)
+        response = requests.get(url, verify=False)
         
         # Verifica se a solicitação foi bem-sucedida (código de status 200)
         if response.status_code == 200:
@@ -28,7 +28,7 @@ def get_company_data(cnpj):
         return None
 
 
-def generate_pdf(data_hora_formatada, razao_social, cnpj, tipo_empresa, abertura, faturamento_mensal_ano_atual, faturamento_mensal_ano_anterior, media_emprestimo, score, score_indice):
+def generate_pdf_empresa(data_hora_formatada, razao_social, cnpj, tipo_empresa, abertura, faturamento_mensal_ano_atual, faturamento_mensal_ano_anterior, media_emprestimo, score, score_indice):
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font("Arial", size=12)
@@ -50,6 +50,24 @@ def generate_pdf(data_hora_formatada, razao_social, cnpj, tipo_empresa, abertura
     pdf.output(pdf_file_path)
     return pdf_file_path
 
+def generate_pdf_pessoa_fisica(data_hora_formatada, nome, cpf, qtd_animais, score_indice, valor_liberado):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+
+    # Dados da pessoa física ao PDF
+    pdf.cell(200, 10, txt="Data do Pedido: " + data_hora_formatada, ln=True)
+    pdf.cell(200, 10, txt="Nome: " + nome, ln=True)
+    pdf.cell(200, 10, txt="CPF: " + cpf, ln=True)
+    pdf.cell(200, 10, txt="Quantidade de Animais: " + qtd_animais, ln=True)
+    pdf.cell(200, 10, txt="Índice de risco: " + score_indice, ln=True)
+    pdf.cell(200, 10, txt="Valor liberado: R$ " + str(valor_liberado), ln=True)
+    
+    # Salva o PDF
+    pdf_file_path = "dados_da_pessoa_fisica.pdf"
+    pdf.output(pdf_file_path)
+    return pdf_file_path
+
 
 # Definição da função
 def calcular_classificacao_e_valor(score):
@@ -61,6 +79,28 @@ def calcular_classificacao_e_valor(score):
         return "Médio", 0.3
     else:
         return "Muito Alto", 0
+
+
+# Função para calcular o SCORE e o risco com base na quantidade de animais
+def calcular_score_e_risco(valor_base):
+    if valor_base >= 1000:
+        return "Baixíssimo", 200000
+    elif valor_base >= 501:
+        return "Baixíssimo", 120000
+    elif valor_base >= 301:
+        return "Baixo", 50000
+    elif valor_base >= 200:
+        return "Baixo", 30000
+    elif valor_base >= 101:
+        return "Médio", 20000
+    elif valor_base >= 50:
+        return "Médio", 10000
+    elif valor_base >= 30:
+        return "Alto", 7000
+    elif valor_base >= 10:
+        return "Alto", 5000
+    else:
+        return "Muito Alto", 0.0
 
 
 def main():
@@ -89,8 +129,8 @@ def main():
         # Calcular a media_emprestimo
         media_emprestimo = total_media * valor
         
-        # Botão para enviar o formulário
-        if st.button("Consultar"):
+        # Botão para enviar o formulário de consulta para empresa
+        if st.button("Consultar Empresa"):
             # Verifica se o CNPJ foi inserido
             if cnpj:
                 # Chamada à função para obter dados da empresa
@@ -117,13 +157,13 @@ def main():
                     st.write('NOVO Limite Liberado PJ: ', media_emprestimo)
                     
                     
-                    # Gera o PDF
-                    pdf_file_path = generate_pdf(data_hora_formatada, razao_social, cnpj, tipo_empresa, abertura, faturamento_mensal_ano_atual, faturamento_mensal_ano_anterior, media_emprestimo, score, score_indice)
+                    # Gera o PDF para empresa
+                    pdf_file_path = generate_pdf_empresa(data_hora_formatada, razao_social, cnpj, tipo_empresa, abertura, faturamento_mensal_ano_atual, faturamento_mensal_ano_anterior, media_emprestimo, score, score_indice)
                     
-                    # Cria o botão de download para o PDF
+                    # Cria o botão de download para o PDF da empresa
                     with open(pdf_file_path, "rb") as f:
                         st.download_button(
-                            label="Baixe o PDF",
+                            label="Baixe o PDF - Empresa",
                             data=f,
                             file_name="dados_da_empresa.pdf",
                             mime="application/pdf"
@@ -134,9 +174,37 @@ def main():
             else:
                 st.warning("Por favor, insira um CNPJ válido.")
 
+    # Para Pessoa Física
+    st.title("Pessoa Física")
+    # Campo de entrada para o CPF
+    nome = st.text_input('Nome')
+    cpf = st.text_input("Por favor, insira o CPF (apenas números):")
+    qtd_animais = st.text_input('Quantidade de Animais')
 
+    
+    if qtd_animais:
+        qtd_animais = int(qtd_animais)
+        score_indice, valor_liberado = calcular_score_e_risco(qtd_animais)
+        
+        st.write(f"Nome: {nome}")
+        st.write(f"Índice de risco: {score_indice}")
+        st.write(f"Valor liberado: R${valor_liberado}")
 
-
+        # Botão de gerar PDF para Pessoa Física
+        if st.button("Gerar PDF - Pessoa Física"):
+            if cpf:
+                pdf_file_path = generate_pdf_pessoa_fisica(data_hora_formatada, nome, cpf, qtd_animais, score_indice, valor_liberado)
+                    
+                # Botão de download do PDF para Pessoa Física
+                with open(pdf_file_path, "rb") as f:
+                    st.download_button(
+                        label="Baixe o PDF - Pessoa Física",
+                        data=f,
+                        file_name="dados_da_pessoa_fisica.pdf",
+                        mime="application/pdf"
+                    )
+            else:
+                st.warning("Por favor, insira um CPF válido.")
 
 if __name__ == "__main__":
     main()
